ce6cea0df52e3e27b0a90f6a61fe6b5a487a4810
mdaga06 <55628156+mdaga06@users.noreply.github.com>
2020-06-20 23:06:06 +0530

    Add files via upload

---
+++ 33741627.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,k,a[100000],i,d=0;
+        scanf("%d%d",&n,&k);
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]); 
+        for(i=0;i<n;i++)
+        if(a[i]>k)
+        {
+        d=d+(a[i]-k);
+        }
+        printf("\n%d",d);
+    }
+    return 0;
+}
+        
